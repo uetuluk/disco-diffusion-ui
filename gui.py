0ba33f4b639d2b0ddaf421e447b11ce08ad9e2b1
uetuluk <uet200@nyu.edu>
2022-10-22 15:36:51 +0800

    feat add preview progress and parameter input

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ import asyncio
 from random import randint 
 
 st.title('Disco Diffusion')
+form = st.form("prompt_form")
 
 # ENV Variables
 HOST_LOCATION = os.environ['SERVER_LOCATION']
@@ -27,8 +28,8 @@ async def disco_request(text_prompts: list, name_docarray: str):
             'batch_size': 1,
             'cutn_batches': 1,
             'n_batches': 1,
-            'steps': 10,
-            'width_height': [500, 500]
+            'steps': st.session_state.steps,
+            'width_height': [st.session_state.width, st.session_state.height]
         },
     ):
         create_response_array.append(resp)
@@ -64,13 +65,14 @@ async def preview_handler(name_docarray: str):
     ):
         # await resp
         print("Run preview handler")
-        print(name_docarray)
-        print(resp)
+        # print(name_docarray)
+        # print(resp)
         print(resp.summary())
         if len(resp) > 0:
-            print(resp[0])
-            print(resp[0].summary())
-            preview_response_array.append(resp)
+            # print(resp[0])
+            document = resp[0]
+            print(document.summary())
+            preview_response_array.append(document)
         # if resp.length > 0:
         #     print(resp[0])
         #     print(resp[0].summary())
@@ -81,13 +83,20 @@ async def preview_handler(name_docarray: str):
 async def preview_handler_wait():
     # preview_task = asyncio.create_task(preview_handler(st.session_state.name_docarray))
     # print("Running the preview handler wait loop")
-    while len(preview_response_array) < 1:
+    completed = False
+    preview_image = form.empty()
+    while len(preview_response_array) < 1 or not completed:
         # print("Waiting for preview response")
         await asyncio.sleep(1)
         await preview_handler(st.session_state.name_docarray)
         # print("current length: " + str(len(preview_response_array)))
         # print("Waiting 1 second")
-        
+        # Add the image to preview
+        if len(preview_response_array) > 0:
+            latest_document = preview_response_array[-1]
+            preview_image.image(image=latest_document.uri)
+            completed = latest_document.tags["_status"]["completed"] is True
+
 
 async def prompt_handler():
     create_task = asyncio.create_task(disco_request(st.session_state.text_prompts, st.session_state.name_docarray))
@@ -99,10 +108,11 @@ async def prompt_handler():
         preview_handler_wait()
     )
 
-    if len(preview_response_array) > 0:
-        st.image(image=preview_response_array[0][0].uri)
-    else:
-        st.write("Error")
+    # if len(preview_response_array) > 0:
+    #     form.image(image=preview_response_array[0][0].uri)
+    # else:
+    if len(preview_response_array) < 1:
+        form.write("Error")
     # await preview_handler_wait()
 
     # done, pending = await asyncio.wait({create_task})
@@ -119,7 +129,11 @@ async def prompt_handler():
 
 
 def click_handler():
-    asyncio.run(prompt_handler())
+    if st.session_state.text_prompts == "":
+        form.error("Please input a prompt")
+        # text_input.help = "Please input a prompt"
+    else:
+        asyncio.run(prompt_handler())
     
     # asyncio.to_thread(blocking_disco_request(st.session_state.text_prompts, st.session_state.name_docarray))
 
@@ -131,12 +145,22 @@ def click_handler():
 
 # Receive the variables with streamlit
 
+# Variables for the GUI
+st.session_state["input_help"] = ""
+
 def main():
-    st.text_input(label="Please input the prompts", key="text_prompts")
+    form.text_input(label="Please input the prompts", key="text_prompts")
+    
+    form.number_input(label="Steps", min_value=50, max_value=200, key="steps")
 
-    st.button(label="Start", on_click=click_handler)
+    form.number_input(label="Width", min_value=300, max_value=500, key="width")
 
-    st.text(st.session_state.name_docarray)
+    form.number_input(label="Height", min_value=300, max_value=500, key="height")
+
+    form.form_submit_button(label="Start", on_click=click_handler)
+
+    form.text("Current Image ID:")
+    form.text(st.session_state.name_docarray)
 
 
 if __name__ == "__main__":
